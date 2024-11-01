17571fcbd573e6e5e821c85044cb644915df215c
gabriel073 <gabrielmarzioli@hotmail.com>
2024-11-01 18:54:32 -0300

    modify acces cors

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const server = express()
 server.use(express.json())
 server.use(
   cors({
-    origin: "https://pikapak.vercel.app",
+    origin: "https://backend-pikapak.vercel.app",
     credentials: true,
   })
 )
